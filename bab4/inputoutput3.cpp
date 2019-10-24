dd16f0b93b363dc16b2a309aeb42a9fe2064bec8
Abdul Jalil Maulani <abduljalilmaulani@gmail.com>
2019-10-24 15:49:17 +0700

    first commit

---
+++ bab4/inputoutput3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+char tampilkan[1];
+char panjang_data[50];
+cout << " ==================================\n";
+cout << "      Belajar pemrograman C++      \n";
+cout << " ==================================\n";
+
+cout << " Nama:";
+cin.getline (panjang_data,50);
+cout << " Jurusan : Teknik Informatika-UNISKA Banjarmasin";
+cin.getline (tampilkan,1);
+
+return 0;
+}
